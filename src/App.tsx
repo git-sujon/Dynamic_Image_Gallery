460c07ff575156f429b852688aaa6b98ada3e510
git-sujon <git.sujon@gmail.com>
2023-11-02 16:46:02 +0600

    Initial design complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import ImageCard from "./components/pages/Homepage/ImageCard/ImageCard"
-import { imageData } from "./contents/imageData"
-
+import ImageCard from "./components/pages/Homepage/ImageCard/ImageCard";
+import { imageData } from "./contents/imageData";
 
 function App() {
-
-
   return (
-    <>
-     <div>
-     {
-        imageData?.map(image => <ImageCard key={image.id} imageInfo={image} />)
-      }
-     </div>
-    </>
-  )
+    <div className="max-w-4xl mx-auto rounded-lg shadow-md mt-10 bg-slate-50 border-slate-700">
+      <div className=" px-8">
+        <div className="flex items-center justify-between border-b  py-3">
+          <p className="text-xl font-bold  mb-0">Simple Gallery</p>
+          <button className="text-red-600 text-lg font-bold hover:text-red-700">
+            Delete files
+          </button>
+        </div>
+        <div className="grid grid-cols-5 gap-5 p-8  border">
+          {imageData?.map((image, index) => (
+            <ImageCard
+              key={image.id}
+              imageInfo={image}
+              // this class wil make the first image size 1x4
+              className={index === 0 ? "col-span-2 row-span-2" : ""}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/pages/Homepage/ImageCard/ImageCard.tsx
+++ src/components/pages/Homepage/ImageCard/ImageCard.tsx
@@ -1,13 +1,27 @@
 import { IImageInfo } from "../../../../types/common";
 
-const ImageCard = ({ imageInfo }: { imageInfo: IImageInfo }) => {
+const ImageCard = ({
+  imageInfo,
+  className,
+}: {
+  imageInfo: IImageInfo;
+  className: string;
+}) => {
   return (
-    <div className="w-64 h-64 bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300 border">
-      <img
-        src={imageInfo?.img}
-        alt={imageInfo?.title}
-        className="w-full h-48 object-cover rounded-lg mb-2"
-      />
+    <div className={`flex justify-center border ${className}`}>
+      <div className=" relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+        <div>
+          <img className="rounded-t-lg" src={imageInfo.img} alt="" />
+        </div>
+        <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black overly-bg bg-opacity-50 opacity-0 hover:opacity-100  font-black">
+          <input
+            type="checkbox"
+            id={imageInfo.id}
+            name={imageInfo.title}
+            className="w-5   h-5"
+          />
+        </div>
+      </div>
     </div>
   );
 };
